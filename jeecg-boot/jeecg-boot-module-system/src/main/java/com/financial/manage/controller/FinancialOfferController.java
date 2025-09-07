484bdae969e92f4d8c25b247538d5ab417e96e71
zhougang <szhougang@gmail.com>
2025-09-07 17:05:14 +0800

    权益改为string

--- jeecg-boot/jeecg-boot-module-system/src/main/java/com/financial/manage/controller/FinancialOfferController.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/com/financial/manage/controller/FinancialOfferController.java
@@ -2,21 +2,14 @@ package com.financial.manage.controller;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import cn.com.dgwl.tools.v2.util.JsonUtil;
 import cn.com.dgwl.tools.v2.util.StringUtil;
-import com.financial.manage.dto.BenefitItemDTO;
 import com.financial.manage.dto.RewardItemDTO;
 import org.jeecg.common.api.vo.Result;
 import org.jeecg.common.system.query.QueryGenerator;
-import org.jeecg.common.util.oConvertUtils;
 import com.financial.manage.entity.FinancialOffer;
 import com.financial.manage.service.IFinancialOfferService;
 
@@ -25,18 +18,10 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.extern.slf4j.Slf4j;
 
-import org.jeecgframework.poi.excel.ExcelImportUtil;
-import org.jeecgframework.poi.excel.def.NormalExcelConstants;
-import org.jeecgframework.poi.excel.entity.ExportParams;
-import org.jeecgframework.poi.excel.entity.ImportParams;
-import org.jeecgframework.poi.excel.view.JeecgEntityExcelView;
 import org.jeecg.common.system.base.controller.JeecgController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
-import com.alibaba.fastjson.JSON;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.jeecg.common.aspect.annotation.AutoLog;
